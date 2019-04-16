308bb4dc96f6d8f4f66f0ebd688a2034364e4f51
Nidhi <nidhixpatel@gmail.com>
2019-04-16 12:22:03 -0700

    added index, show, add new for passengers

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,3 +1,13 @@
 class Passenger < ApplicationRecord
   has_many :trips
+
+  def amount_charged
+    sum = 0
+    self.trips.each do |trip|
+      sum += trip.cost
+    end
+    return sum
+  end
+
+
 end
