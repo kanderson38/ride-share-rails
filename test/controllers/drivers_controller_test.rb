97e011902ea2c3e2cc941b7736b0bfe305349f99
Nidhi <nidhixpatel@gmail.com>
2019-04-15 17:00:20 -0700

    started drivers controller test

--- test/controllers/drivers_controller_test.rb
+++ test/controllers/drivers_controller_test.rb
@@ -1,18 +1,35 @@
 require "test_helper"
 
 describe DriversController do
+
+  let (:driver) {
+    Driver.create(name: "Tom", vin: "123456")
+  }
+
   describe "index" do
     it "can get index" do
-      # Your code here
+         get drivers_path
+         must_respond_with :success
+    end
+
+    it "can get the root path" do
+      get root_path
+      must_respond_with :success
     end
   end
 
   describe "show" do
-    # Your tests go here
+    it "can get a valid task" do
+      get driver_path(driver.id)
+      must_respond_with :success
+    end
   end
 
   describe "edit" do
-    # Your tests go here
+    it "can get the edit page for an existing task" do
+      get edit_driver_path(driver.id)
+      must_respond_with :success
+    end
   end
 
   describe "update" do
