e34a2dc936ed658b85c7f9571f4e049f906afd62
Kirsten Anderson <kirsten@kirstenmunson.com>
2019-04-16 16:30:50 -0700

    styled drivers view

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   root "homepages#index"
 
-  resources :drivers
+  resources :drivers do
+    resources :trips, only: [:index, :new]
+  end
   resources :trips
-  resources :passengers
+  resources :passengers do
+    resources :trips, only: [:index, :new]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
