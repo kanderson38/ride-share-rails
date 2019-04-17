186ad547619288225b87d0765199c8143c0494c1
Kirsten Anderson <kirsten@kirstenmunson.com>
2019-04-17 12:11:34 -0700

    Added show action and view for trip

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   end
   resources :trips
   resources :passengers do
-    resources :trips, only: [:index, :new]
+    resources :trips, only: [:create]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
