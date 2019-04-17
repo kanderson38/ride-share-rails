5f1e87c799cd7f8b5700ab69a2b9fc600d0c5d63
Kirsten Anderson <kirsten@kirstenmunson.com>
2019-04-17 13:39:14 -0700

    edit and show trip

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -8,4 +8,14 @@ class TripsController < ApplicationController
       head :not_found
     end
   end
+
+  def edit
+    trip_id = params[:id]
+
+    @trip = Trip.find_by(id: trip_id)
+
+    unless @trip
+      head :not_found
+    end
+  end
 end
